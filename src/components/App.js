841fdb928178872758082b31835ac65488e2fc5a
sajid <mohdsajid8010@gmail.com>
2023-07-19 00:12:03 +0530

    commit3

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
-const App = () => {
-  const [complete,setComplete]=useState({Learn:false,Build:false,Deploye:false});
-
+import TodoList from "./TodoList";
+ 
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false }
+  ]);
+ 
+  const handleComplete = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>Parent component</h1>
-        <div>
-          <h1>Child Component</h1>
-          <ul>
-            <li>
-              Learn React {!complete.Learn && (<button onClick={()=>setComplete({...complete,Learn:true})}>Complete</button>) }
-            </li>
-            <li>
-            Build a React app {!complete.Build && (<button onClick={()=>setComplete({...complete,Build:true})}>Complete</button>) }
-            </li>
-            <li>
-            Deploy the React app {!complete.Deploye && (<button onClick={()=>setComplete({...complete,Deploye:true})}>Complete</button>) }
-            </li>
-          </ul>
-        </div>
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
+  );
 }
-
-export default App
+ 
+export default App;
